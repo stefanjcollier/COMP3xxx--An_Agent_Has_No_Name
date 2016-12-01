5763ef0a85cc31a86d21ed5106312f76190ccef5
Stefan Collier <sc22g13@soton.ac.uk>
2016-12-01 20:09:36 +0000

    Finished Publisher User Income Stats

--- src/org/dank/tables/publisher/PublisherIncomeStatistics.java
+++ src/org/dank/tables/publisher/PublisherIncomeStatistics.java
@@ -1,5 +1,8 @@
 package org.dank.tables.publisher;
 
+import java.util.StringJoiner;
+import java.util.stream.DoubleStream;
+
 /**
  * Created by Stefa on 01/12/2016.
  */
@@ -9,28 +12,28 @@ public class PublisherIncomeStatistics {
     public static final int inc60_100 = 0;
     public static final int inc100plus = 0;
 
-    public final double[][] chances_of_incomes = new double[][]
+    protected final double[][] chances_of_incomes = new double[][]
             {
                     {0.53, 0.27, 0.13, 0.07},
-                    {},
-                    {},
-                    {},
-                    {},
-                    {},
+                    {0.48, 0.27, 0.16, 0.09},
+                    {0.47, 0.26, 0.17, 0.10},
+                    {0.47, 0.27, 0.17, 0.09},
+                    {0.49, 0.27, 0.16, 0.08},
+                    {0.46, 0.26, 0.18, 0.10},
 
-                    {},
-                    {},
-                    {},
-                    {},
-                    {},
-                    {},
+                    {0.50, 0.27, 0.15, 0.08},
+                    {0.50, 0.27, 0.15, 0.08},
+                    {0.47, 0.28, 0.19, 0.06},
+                    {0.45, 0.27, 0.19, 0.09},
+                    {0.465, 0.27, 0.19, 0.075},
+                    {0.45, 0.25, 0.20, 0.10},
 
-                    {},
-                    {},
-                    {},
-                    {},
-                    {},
-                    {},
+                    {0.46, 0.265, 0.185, 0.09},
+                    {0.5, 0.27, 0.15, 0.08},
+                    {0.5, 0.28, 0.15, 0.07},
+                    {0.465, 0.26, 0.175, 0.1},
+                    {0.48, 0.265, 0.165, 0.09},
+                    {0.455, 0.265, 0.185, 0.095},
             };
 
     public double has0_30Income(PublisherToken publisher){
@@ -52,4 +55,22 @@ public class PublisherIncomeStatistics {
         return this.chances_of_incomes[publisher.getIndex()];
     }
 
+
+    public static void main(String args[]){
+
+        int index = 0;
+        boolean all_good = true;
+        for (double[] row : new PublisherIncomeStatistics().chances_of_incomes){
+            double sum = DoubleStream.of(row).sum();
+            if (sum != 1.0){
+                System.out.println(String.format("Row %d has a sum of %f",index,sum));
+                all_good = false;
+            }else {
+                System.out.println(String.format("Row %d good",index));
+            }
+            index++;
+        }
+        System.out.println("Done: "+((all_good)?"All Good!":"Failures"));
+
+    }
 }
