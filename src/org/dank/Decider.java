c3055572e085467a0d70bca0322a1e9151e36f10
Stefan Collier <sc22g13@soton.ac.uk>
2016-12-02 17:33:01 +0000

    Outlined the 2decision factors for CampaignDecider

--- src/org/dank/Decider.java
+++ src/org/dank/Decider.java
@@ -14,15 +14,22 @@ import java.util.Collection;
 import java.util.Set;
 
 /**
+ * A implementation of {@link CampaignDecider} that determines if the campaign should be
+ * bid on based on two criteria:
+ *
+ *  1. There will be enough impressions for the campaign over the entire duration of the campaign
+ *  2. For each day of the campaign, there is a big enough gap in the market for the new campaign
+ *
  * Created by EKREM on 1.12.2016.
+ * Edited by: Ekrem, Stefan
  */
 public class Decider implements CampaignDecider {
 
     private MarketMonitor monitor;
     private UserPopulations userPopulations;
     private int currentDay;
-    public Decider(MarketMonitor monitor){
 
+    public Decider(MarketMonitor monitor){
         this.monitor = monitor;
         userPopulations = new UserPopulations();
 
@@ -44,5 +51,30 @@ public class Decider implements CampaignDecider {
 
         return false;
     }
+
+    /**
+     *
+     * Given total number of impressions generated over the length of the campaign;
+     * is that enough for the incoming campaign?
+     *
+     * @param incomingCamp -- The campaign we are decing whether to accept
+     * @return true -- There is enough impressions for the incomingCamp
+     */
+    protected boolean isEnoughImpressionsForNewCampaign(Campaign incomingCamp){
+        return true;
+    }
+
+    /**
+     * By iterating over each day of the campaign and summing the required impressions (per day)
+     * of each campaign with overlapping market segments. Is there enough remaining impressions (per day)
+     * to support the incoming campaign.
+     *
+     * @param incomingCamp -- The campaign we are decing whether to accept
+     * @return true -- There is a enough
+     */
+    protected boolean isEnoughRemainingImpressionsPerDay(Campaign incomingCamp){
+        return true;
+    }
+
 
 }
