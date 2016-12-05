2ade75e365b18863d25a71fd94b4183794b5a408
ekremyurdakul <ekrem@yurdakul.net>
2016-12-05 01:52:17 +0000

    ucsbidder - incomplete

--- src/org/dank/bidder/UCSBidder.java
+++ src/org/dank/bidder/UCSBidder.java
@@ -7,6 +7,7 @@ public class UCSBidder {
 
     private static UCSBidder INSTANCE;
 
+
     private UCSBidder() {};
 
 
@@ -16,5 +17,28 @@ public class UCSBidder {
             INSTANCE = new UCSBidder();
         }
         return INSTANCE;
-    };
+    }
+
+
+    public long calcUCSBid(long prevBid,long ucsLevel,int currentDay){
+        long estimatedImpressionReach = 10000; //todo -- the expected impressions to go in the next simulated day
+        long gucs = (long) 0.5; //todo --  constant
+        long Ep = 10000;//todo -- p:= impression unit-price
+
+        long r0 = (long) 0.75 * (estimatedImpressionReach);
+
+        if(ucsLevel > 0.9){
+
+            return prevBid / (1+gucs);
+        }else if(ucsLevel < 0.81 && ((r0 / prevBid) >= (20 / 3) * ((1+gucs)/(Ep)))){
+
+            return (1+gucs) / prevBid;
+        }else{
+
+            return prevBid;
+        }
+
+    }
+
 }
+
