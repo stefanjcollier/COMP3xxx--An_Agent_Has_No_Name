fce3faa58dfce8c84c6ab2898b570c73361d1b00
Vlad Catrici <vlad@Vlads-MacBook-Pro.local>
2016-12-05 03:29:53 +0000

    Added PriceIndexPredictor, CampaignBidder and State

--- src/org/State.java
+++ src/org/State.java
@@ -11,9 +11,51 @@ public class State {
 
     private static State INSTANCE;
 
-    private State() {};
+    private double ci;
+    private final double gGreed;
 
+    public static final double LOW_QUALITY = 0.8;
 
+    public static final double PI_TOO_HIGH = 9.0; //TODO determine this
+
+    public enum BidOutcome { BID_WIN, RANDOM_WIN, LOSS }
+
+    private State() {
+        this.ci = 1.0;
+        this.gGreed = 1.2;
+    }
+
+    public double getLOW_QUALITY() {
+        return LOW_QUALITY;
+    }
+
+    public double getCi() {
+        return ci;
+
+    }
+
+    public double getgGreed() {
+        return gGreed;
+    }
+
+    public void updateCi(BidOutcome outcome){
+        switch(outcome){
+            case BID_WIN: {
+                // Ci remains the same
+                break;
+            }case RANDOM_WIN:{
+                this.ci /= this.gGreed;
+                break;
+
+            }case LOSS:{
+                this.ci *= this.gGreed;
+                break;
+
+            }default:{
+                throw new RuntimeException("Unrecognised Contract BidOutcome enum used: "+outcome.toString());
+            }
+        }
+    }
 
     public static State getInstance(){
         if (INSTANCE == null) {
--- src/org/dank/bidder/CampaignBidder.java
+++ src/org/dank/bidder/CampaignBidder.java
@@ -1,20 +1,74 @@
 package org.dank.bidder;
 
+import org.State;
+import org.dank.bidder.index.PriceIndexPredictor;
+import org.dank.entities.Campaign;
+
 /**
+ *  This class determines the value of the bid for any given bid
+ *
  * Created by vlad on 04/12/2016.
  */
 public class CampaignBidder {
 
     private static CampaignBidder INSTANCE;
 
-    private CampaignBidder() {};
+    PriceIndexPredictor priceIndexPredictor;
+    State state;
+
+    public CampaignBidder() {
+        this.priceIndexPredictor = PriceIndexPredictor.getInstance();
+        this.state = State.getInstance();
+    }
 
 
+    public double getBidFor(Campaign incomingCamp, double myQuality){
+        // We prioritise having a high quality
+        if (myQuality <= State.LOW_QUALITY){
+            return performStrategy2(incomingCamp, myQuality);
 
-    public static CampaignBidder getInstance(){
-        if (INSTANCE == null) {
-            INSTANCE = new CampaignBidder();
+        } else if (!this.isCampaignAchievable(incomingCamp)) {
+            return performStrategy3(incomingCamp, myQuality);
+
+        } else {
+            return performStrategy1(incomingCamp);
         }
-        return INSTANCE;
-    };
+    }
+
+    /**
+     * bid private value: PI * CI
+     * (Normal Path)
+     * */
+    protected double performStrategy1(Campaign incomingCamp){
+        return this.pici(incomingCamp);
+    }
+    /** Bid lowest valid price */
+    protected double performStrategy2(Campaign incomingCamp, double myQuality){
+        return this.lowestPrice(incomingCamp, myQuality);
+    }
+    /** Bid highest valid price */
+    protected double performStrategy3(Campaign incomingCamp, double myQuality){
+        return this.highestPrice(incomingCamp, myQuality);
+    }
+
+
+    private double pici(Campaign incomingCamp){
+        double pi = this.priceIndexPredictor.estimatePriceForSegmentMultipleDays(incomingCamp);
+        double ci = this.state.getCi();
+        return pi * ci;
+    }
+
+    /** if budget (PI*CI) > reach**/
+    private boolean isCampaignAchievable(Campaign incomingCamp){
+        double pi = this.priceIndexPredictor.estimatePriceForSegmentMultipleDays(incomingCamp);
+        return pi >= State.PI_TOO_HIGH;
+    }
+
+    private double lowestPrice(Campaign incomingCamp, double myQuality){
+        return incomingCamp.getReachImps() / myQuality;
+    }
+
+    private double highestPrice(Campaign incomingCamp, double myQuality){
+        return (incomingCamp.getReachImps() * myQuality) / 10.0;
+    }
 }
