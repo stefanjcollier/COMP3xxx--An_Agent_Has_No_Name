be55c9a4ebe5c8a634bc990cb96d2bb2fc8158ba
Stefan Collier <sc22g13@soton.ac.uk>
2016-12-05 11:37:39 +0000

    Fixed classpath

--- runAgent.bat
+++ runAgent.bat
@@ -8,4 +8,4 @@ TACAA_HOME=%pwd%
 echo %TACAA_HOME%
 echo %CLASSPATH%
 
-java -cp "lib/*:out/artifacts/*" tau.tac.adx.agentware.Main -config config/aw-1.conf
+java -cp "lib/*;out/artifacts/*" tau.tac.adx.agentware.Main -config config/aw-1.conf
